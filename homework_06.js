d106fa9d30f7c146991070c6395875d1b93161a8
mv-ubuntu14 <dirosaclemente96@gmail.com>
2015-11-22 18:37:23 +0100

    aggiunta homework_06.js

---
+++ homework_06.js
@@ -0,0 +1,44 @@
+function CircularQueue(n){
+	this.myarray = new Array(n);
+	this.coda = 0;
+	this.testa = 0;
+	var cap = n;
+	var dim = 0;
+	
+		
+	
+}
+
+CircularQueue.prototype.enqueue = function(e){
+	if(this.dim == this.n){
+		return;
+	}
+	this.myarray[this.coda] = e;
+	this.coda = (this.coda + 1) % this.n;
+	this.dim++;
+	
+}
+CircularQueue.prototype.dequeue = function(){
+	if(this.isEmpty()){
+		return null;
+	}
+	var temp = this.myarray[this.testa];
+	this.myarray[testa] = null;
+	this.testa = (this.testa + 1) % this.n;
+	this.dim--;
+	return temp;
+}
+CircularQueue.prototype.front = function(){
+	return this.myarray[this.testa];
+}
+CircularQueue.prototype.isEmpty = function(){
+	return this.size() == 0;
+
+}
+
+CircularQueue.prototype.size = function(){
+	return this.dim;
+}
+
+
+
