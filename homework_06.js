6be834779690c4f55ab12fe94f8fecffd5e6bef0
ClementeDR <dirosaclemente96@gmail.com>
2015-11-24 14:01:07 +0100

    Update homework_06.js
    update circularQueue

--- homework_06.js
+++ homework_06.js
@@ -1,21 +1,22 @@
 function CircularQueue(n){
-	this.myarray = new Array(n);
+	this.myarray = [n];
 	this.coda = 0;
 	this.testa = 0;
-	var cap = n;
-	var dim = 0;
+	this.cap = n;
+	this.dim = 0;
 	
 		
 	
 }
 
 CircularQueue.prototype.enqueue = function(e){
-	if(this.dim == this.cap){
+	if(this.size() == this.cap){
 		return false;
 	} else {
 		this.myarray[this.coda] = e;
 		this.coda = (this.coda + 1) % this.cap;
 		this.dim++;
+		
 	}
 }
 CircularQueue.prototype.dequeue = function(){
@@ -23,7 +24,7 @@ CircularQueue.prototype.dequeue = function(){
 		return null;
 	} else {
 		var temp = this.myarray[this.testa];
-		this.myarray[testa] = null;
+		this.myarray[this.testa] = null;
 		this.testa = (this.testa + 1) % this.cap;
 		this.dim--;
 		return temp;
@@ -33,7 +34,7 @@ CircularQueue.prototype.front = function(){
 	return this.myarray[this.testa];
 }
 CircularQueue.prototype.isEmpty = function(){
-	return this.size() == 0;
+	return this.size() === 0;
 
 }
 
