6bc42c64d9edd59e250c0b8c769a2d3c2352f283
mv-ubuntu14 <dirosaclemente96@gmail.com>
2015-12-01 15:06:37 +0100

    8.0.1

--- homework_08.js
+++ homework_08.js
@@ -18,3 +18,12 @@ function merge(a,b){
 	
 }
 
+
+function mergesort(myarray){
+	if(myarray.length == 1)
+		return myarray;
+
+	var mid = Math.ceil(myarray.length / 2);
+	return merge(mergesort(myarray.slice(0, mid)), mergesort(myarray.slice(mid)));
+}
+
